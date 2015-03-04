f5389a77bb043385975133dd9df1a90b36cc7f60
Trond Tufte <trondtufte89@gmail.com>
2015-03-04 16:44:16 +0100

    :)

--- AndAlgorithm/src/Main.java
+++ AndAlgorithm/src/Main.java
@@ -1,22 +1,23 @@
 import java.io.File;
 import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.TreeMap;
-
-import com.sun.org.apache.xalan.internal.xsltc.compiler.sym;
 
 /*
  For each query term t
  1. retrieve lexicon entry for t
  2. note ft and address of It (inverted list)
  2. Sort query terms by increasing ft
-
-
-
  3. Initialize candidate set C with It of the term with the smallest ft'
  4. For each remaining t
-
  1. Read It
  2. For each d ϵ C, if d ɇ It, C <- C – {d}
  3. If C = {}, return… there are no relevant docs
@@ -27,81 +28,89 @@ import com.sun.org.apache.xalan.internal.xsltc.compiler.sym;
 public class Main {
 
 	public static boolean[][] check;
-	
+
 	public static void main(String[] args) {
 		TreeMap<String, String[]> col = readFile();
-		System.out.println(search(col, "Trond Andreas sexy"));
-		printCheck();
-		
+		search(col, "B");
+
 	}
 
-	
-	
-	public static String search(TreeMap<String, String[]> col,String searchParam) {
+	public static void search(TreeMap<String, String[]> col, String searchParam) {
 		searchParam = searchParam.replaceAll("[^\\p{L}\\p{Nd}]+", " ");
 		searchParam = searchParam.toLowerCase();
 		String[] search = searchParam.split(" ");
-		TreeMap<Integer, String> docFreq = new TreeMap<Integer, String>();
-		
-		
-		 check = new boolean[col.size()][search.length];
-		 
-		 System.out.println(col.size());
+		HashMap<String, Integer> docFreq;
+		docFreq = new HashMap<String, Integer>();
+		check = new boolean[col.size()][search.length];
 
-		// Documents
-		int d =0 ;
-	 for (Entry<String, String[]> index : col.entrySet()) {
+		int d = 0;
+		for (Entry<String, String[]> index : col.entrySet()) {
 			String key = index.getKey();
 			String[] values = index.getValue();
 			int freq = 0;
-			System.out.println(key);
-			
+
 			for (String ind : values) {
-				
-				for (int i = 0; i<search.length;i++) {
+				for (int i = 0; i < search.length; i++) {
 					if (search[i].equals(ind)) {
 						freq++;
-					check[d][i] = true;
-					System.out.println("doc "  + d + " " + i + " term");
+						// System.out.println("document: " + key + " " +
+						// " Term: " + ind + " " + " search term: " +
+						// search[i]);
+						check[d][i] = true;
+
 					}
-					
+
 				}
 
 			}
-			
-			docFreq.put(freq, key);
+
+			boolean notIn = false;
+			for (int j = 0; j < check[d].length; j++) {
+				if (check[d][j] == false)
+					notIn = true;
+			}
+
+			if (notIn == false) {
+				docFreq.put(key, freq);
+
+			}
+
 			d++;
 
 		}
 
-		for (Entry<Integer, String> entry : docFreq.entrySet()) {
-			Integer key = entry.getKey();
-			String val = entry.getValue();
-			System.out.println(key + " " + val);
-			
-			
-			for(int i =0;i<check.length;i++){
-				System.out.println(i);
-				for(int j= 0;j< check[i].length;j++){
-					System.out.print(check[i][j] + " ");
-			
-				}
-			}
-			
-		}
 
-		if (docFreq.containsKey(0)) {
-			return null;
-		} else
-			return docFreq.lastEntry().toString();
+		List<Integer> sortList = new ArrayList<Integer>(docFreq.values());
+		Collections.sort(sortList, new Comparator<Integer>() {
+			@Override
+			public int compare(Integer tall1, Integer tall2) {
+				return -(tall1 - tall2);
+			}
+		});
+
+		ArrayList<String> resultSet = new ArrayList<String>();
+		int tall =0;
+		while(!docFreq.isEmpty()){
+		Map.Entry<String,Integer> maxEntry = null;
+		for (Entry<String, Integer> entry : docFreq.entrySet()) {
+				 if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0){
+					 maxEntry = entry;
+				    }
+
+		}
+		
+		resultSet.add(maxEntry.getKey());
+		docFreq.remove(maxEntry.getKey());
+	
+		System.out.println(resultSet.get(tall++));
+	}
+
 	}
 
 	public static TreeMap<String, String[]> readFile() {
 		TreeMap<String, String[]> collection = new TreeMap<String, String[]>();
 		try {
-
 			File[] documents = getDocuments();
-
 			for (File d : documents) {
 				Scanner scan = new Scanner(d);
 				String[] document = null;
@@ -133,16 +142,6 @@ public class Main {
 				return filename.endsWith(".txt");
 			}
 		});
-	}
-	//sadfsdf
-	public static void printCheck(){
-		for(int i = 0;i < check.length;i++){
-			System.out.println("");
-			System.out.print((i+1) +  " : ");
-			for(int j = 0; j< check[i].length;j++){
-				System.out.print(check[i][j] + " ");
-			}
-		}
 	}
 
 }
